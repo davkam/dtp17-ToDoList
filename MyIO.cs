4371c0151e134584172d150ddd6af59b01281f27
davkam <david.jr.kamran@gmail.com>
2022-11-18 17:13:52 +0100

    . added documentation and to-do remarks

--- MyIO.cs
+++ MyIO.cs
@@ -9,19 +9,49 @@ using System.Threading.Tasks;
 
 namespace dtp15_todolist
 {
+    /// <summary>
+    /// <b>MyIO</b> class.
+    /// Contains public methods for use in other classes.
+    /// </summary>
+        // TBD: rework method algorithms for improved user input features.
     class MyIO
     {
+        /// <summary>
+        /// <b>ReadCommand</b> method.
+        /// Takes a string to write to console, reads response from console and splits it into an array.
+        /// </summary>
+        /// <param name="commandPrompt">string.</param>
+        /// <returns>string array (user commands).</returns>
+            // TBD: rework string split so it does not split sentences inside citation marks.
         static public string[] ReadCommand(string commandPrompt)
         {
             Console.Write(commandPrompt);
             string[] commandLines = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
             return commandLines;
         }
+
+        /// <summary>
+        /// <b>CheckFirstCommand</b> method.
+        /// Takes two arguments (string array, and string) and compares array to string.
+        /// If array contains an equal string element it returns true, otherwise false.
+        /// </summary>
+        /// <param name="actualCommand">string[].</param>
+        /// <param name="expectedCommand">string.</param>
+        /// <returns>bool (true or false).</returns>
         static public bool CheckFirstCommand(string[] actualCommand, string expectedCommand)
         {
             if (actualCommand[0].ToLower() == expectedCommand) return true;
             else return false;
         }
+
+        /// <summary>
+        /// <b>CheckAdditionalCommands</b> method
+        /// Takes two arguments (string array, and string) and compares array to string.
+        /// If array contains an equal string element it returns true, otherwise false.
+        /// </summary>
+        /// <param name="actualCommand">string[].</param>
+        /// <param name="expectedCommand">string.</param>
+        /// <returns>bool (true or false).</returns>
         static public bool CheckAdditionalCommands(string[] actualCommand, string expectedCommand)
         {
             bool returnBool = false;
@@ -36,6 +66,15 @@ namespace dtp15_todolist
             }
             return returnBool;
         }
+
+        /// <summary>
+        /// <b>CheckCommandTaskName</b> method.
+        /// Takes two string array arguments, and compares array to array.
+        /// If the arrays contain at least one equal element it returns true, otherwise false.
+        /// </summary>
+        /// <param name="actualCommand">string[].</param>
+        /// <param name="expectedCommand">string[].</param>
+        /// <returns>bool (true or false).</returns>
         static public bool CheckCommandTaskName(string[] actualCommand, string[] expectedCommand)
         {
             bool returnBool = false;
@@ -52,6 +91,15 @@ namespace dtp15_todolist
             }
             return returnBool;
         }
+
+        /// <summary>
+        /// <b>CheckCommandTaskIndex</b> method.
+        /// Takes two string array arguments, and compares array to array.
+        /// If the arrays contain at least one equal element it returns the index array, otherwise returns integer set to 0.
+        /// </summary>
+        /// <param name="actualCommand">string[].</param>
+        /// <param name="expectedCommand">string[].</param>
+        /// <returns>integer (default = 0).</returns>
         static public int CheckCommandTaskIndex(string[] actualCommand, string[] expectedCommand)
         {
             int returnInt = 0;
@@ -68,6 +116,16 @@ namespace dtp15_todolist
             }
             return returnInt;
         }
+
+        /// <summary>
+        /// <b>CheckIndexRange</b> method.
+        /// Takes three integer values, and compares if integer value is between min and max.
+        /// If not, throws an exception!
+        /// </summary>
+        /// <param name="value">integer.</param>
+        /// <param name="min">integer.</param>
+        /// <param name="max">integer.</param>
+        /// <exception cref="Exception">Index out of range!</exception>
         static public void CheckIndexRange(int value, int min, int max)
         {
             if (value < min || value > max) throw new Exception("Index out of range!");
