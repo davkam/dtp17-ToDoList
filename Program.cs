0d92d512c3e226191c1fc33b28229406392ec46a
davkam <david.jr.kamran@gmail.com>
2022-11-18 03:22:51 +0100

    Added new 'list' functionalities and more..

--- Program.cs
+++ Program.cs
@@ -1,26 +1,32 @@
 ﻿using System.Diagnostics;
-using static System.Net.Mime.MediaTypeNames;
+//using static System.Net.Mime.MediaTypeNames;
 
 namespace dtp15_todolist
 {
     class Program
     {
-        public string[] primaryCommands;
-        public string[] secondaryCommands;
-        public string[] tertiaryCommands;
+        public static string[] taskNameCommands;
+        public static void PrintWelcome()
+        {
+            Console.WriteLine("..................ABOUT...................\r\n");
+            Console.WriteLine(". Welcome to To-Do List!");
+            Console.WriteLine(". This app was created for educational purposes.");
+            Console.WriteLine(". To start off, enter the commands given below.\r\n");
+        }
         public static void PrintHelp()
         {
             Console.WriteLine(".................COMMANDS.................\r\n");
             Console.WriteLine($"- {"help", -30}List all commands.");
             Console.WriteLine($"- {"list...   (-d)", -30}List all active tasks in to-do list, with or without description.");
             Console.WriteLine($"  {"   ...all (-d)", -30}List all tasks in to-do list, with or without description.");
+            Console.WriteLine($"  {"   ...\"task name\"",-30}List specific task in to-do list with description.");
             Console.WriteLine($"  {"   ...help",-30}Show possible \"list\" commands.");
             Console.WriteLine($"- {"status...",-30}");
             Console.WriteLine($"  {"   ...set",-30}Show all tasks, and change status on chosen task.");
             Console.WriteLine($"  {"   ...\"status\"",-30}Change status of specific task, (active, waiting or ready).");
             Console.WriteLine($"  {"   ...\"status\" \"task name\"",-30}Change status of specific task, (active, waiting or ready).");
             Console.WriteLine($"- {"new...", -30}Add new task to to-do list.");
-            Console.WriteLine($"  {"   ...\"task name\"",-30}Add new task to to-do list, and initialize with a task name");
+            Console.WriteLine($"  {"   ...\"task name\"",-30}Add new task to to-do list, and initialize with a task name"); // delete, clear, change 
             Console.WriteLine($"- {"load",-30}Load to-do list from \"/todo.lis\".");
             Console.WriteLine($"- {"save", -30}Save current to-do list to \"/todo.lis\".");
             Console.WriteLine($"- {"quit", -30}Quit and save to-do list.");
@@ -42,6 +48,9 @@ namespace dtp15_todolist
         {
             Console.WriteLine("................TO-DO LIST................\r\n");
             Todo.ReadListFromFile();
+            taskNameCommands = new string[Todo.todoList.Count];
+            Todo.AddTaskNamesToCommand();
+            PrintWelcome();
             PrintHelp();
             string[] commandLines;
             do
@@ -58,19 +67,28 @@ namespace dtp15_todolist
                         if (MyIO.CheckAdditionalCommands(commandLines, "-d")) Todo.PrintTodoList(allTasks: true, verbose: true);
                         else Todo.PrintTodoList(allTasks: true, verbose: false);
                     }
+                    else if (MyIO.CheckCommandTaskName(commandLines, taskNameCommands))
+                    {
+                        int taskIndex = MyIO.CheckCommandTaskIndex(commandLines, taskNameCommands);
+                        Todo.PrintHead(true);
+                        Todo.todoList[taskIndex].Print(true, taskIndex);
+                        Todo.PrintFoot(true);
+                    }
                     else if (MyIO.CheckAdditionalCommands(commandLines, "help"))
                     {
                         Console.WriteLine(". Possible \"list\" commands...");
                         Console.WriteLine($"- {"list",-20}Shows all active tasks without description.");
-                        Console.WriteLine($"- {"list +d",-20}Shows all active tasks with description.");
+                        Console.WriteLine($"- {"list -d",-20}Shows all active tasks with description.");
                         Console.WriteLine($"- {"list all",-20}Shows all tasks without description.");
-                        Console.WriteLine($"- {"list all +d",-20}Shows all tasks with description.");
+                        Console.WriteLine($"- {"list all -d",-20}Shows all tasks with description.");
+                        Console.WriteLine($"- {"list \"task name\"",-20}Shows specific task with description.");
                     }
-                    else
+                    else if (MyIO.CheckAdditionalCommands(commandLines, "-d") || commandLines.Length <= 1)
                     {
                         if (MyIO.CheckAdditionalCommands(commandLines, "-d")) Todo.PrintTodoList(allTasks: false, verbose: true);
                         else Todo.PrintTodoList(allTasks: false, verbose: false);
                     }
+                    else UnknownCommand(commandLines);
                 }
                 else if (MyIO.CheckFirstCommand(commandLines, "status"))
                 {
