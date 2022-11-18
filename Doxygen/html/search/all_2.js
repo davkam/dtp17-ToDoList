d860b863f80491b6697819e87af990473c0fb3c6
davkam <david.jr.kamran@gmail.com>
2022-11-18 18:44:38 +0100

    . doxygen updates

--- Doxygen/html/search/all_2.js
+++ Doxygen/html/search/all_2.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['deletetask_0',['DeleteTask',['../classdtp15__todolist_1_1_todo.html#a7f297892c116b1bf6c0f3801a179fe5a',1,'dtp15_todolist::Todo']]],
-  ['dtp15_5ftodolist_1',['dtp15_todolist',['../namespacedtp15__todolist.html',1,'dtp15_todolist'],['../md__r_e_a_d_m_e.html',1,'(Global Namespace)']]]
+  ['dtp15_5ftodolist_1',['dtp15_todolist',['../namespacedtp15__todolist.html',1,'']]],
+  ['dtp17_5ftodolist_20_28hemtentamen_29_2',['dtp17_todolist (hemtentamen)',['../md__r_e_a_d_m_e.html',1,'']]]
 ];
